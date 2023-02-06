00a8a0fab5575572bf9ff85dcb53a9d4f3d2ba12
Rahul Dayal <rdayal@TSDWEB.COM>
2023-02-06 15:35:07 -0500

    RD : First Commit

---
+++ youtube-api/README.md
@@ -0,0 +1,4 @@
+# YouTube Clone
+1. Used Axios for API Integration
+2. Used Material UI -V 4.3.0
+3. 
---
+++ youtube-api/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+class App extends React.Component{
+    render() {
+        return (<h1>
+            YouTube Clone App
+        </h1>)
+    }
+}
