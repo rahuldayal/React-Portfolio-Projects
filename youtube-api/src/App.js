48a4eb90ea813e906cf8c8b989e3f0c7700aa0a5
Rahul Dayal <rdayal@TSDWEB.COM>
2023-02-07 11:40:35 -0500

    RD : first commit

--- youtube-api/src/App.js
+++ youtube-api/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from "react";
+
+import {
+    Grid
+} from "@mui/material";
+
+import youtube from './api/youtube'
 class App extends React.Component{
     render() {
-        return (<h1>
-            YouTube Clone App
-        </h1>)
+        return (
+          <Grid justify="center" container spacing={16}>
+            <Grid item xs={12}>
+              <Grid container spacing={16}>
+                <Grid item xs={12}>
+                  {/*Search bar */}
+                </Grid>
+                <Grid item xs={8}>
+                  {/*Video Details */}
+                </Grid>
+                <Grid item xs={4}>
+                  {/*Video List */}
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        );
     }
 }
+
+export default App
